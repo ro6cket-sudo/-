07170ace1a45f1464d70008620346cb2fdc77cb3
ro6cket-sudo <ro6cket@gmail.com>
2026-07-03 21:28:54 +0700

    написал нахождение приходящих дней рождения

---
+++ js/Notification.js
@@ -0,0 +1,43 @@
+class Notification {
+    constructor(userRepository, groupRepository, subcriptionRepository) {
+        this.userRepository = userRepository;
+        this.groupRepository = groupRepository;
+        this.subcriptionRepository = subcriptionRepository;
+    }
+
+    getUpcomingBirthdays(id){
+        const subs = this.subcriptionRepository.getUserSubcriptions(id);
+        const targetUsersIds = new Set();
+
+        subs.forEach((sub) => {
+            if (sub.targetUserId) targetUsersIds.add(sub.targetUserId);
+            if (subs.targetGroupId) {
+                const group = this.groupRepository.getById(sub.targetGroupId);
+                if (group) group.members.forEach(member => {targetUsersIds.add(member)});
+            }
+        })
+        targetUsersIds.delete(id);
+
+        const upcomingBirthdays = [];
+        const today = new Date();
+        today.setHours(0, 0, 0, 0);
+
+        targetUsersIds.forEach((id) => {
+            const user = this.userRepository.getById(id);
+            if (!user) return;
+             const [y, m, d] = user.birthday.split('-').map(Number);
+             let nextBirthday = new Date(today.getFullYear(), m - 1, d)
+             if (nextBirthday < today){
+                 nextBirthday = new Date(today.getFullYear() + 1, m - 1, d)
+             }
+
+             const diffTime = Math.abs(nextBirthday - today);
+             const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+             if (diffDays <= 7) {
+                 upcomingBirthdays.push(nextBirthday);
+             }
+        })
+        return upcoming.sort((a, b) => a.daysLeft - b.daysLeft);
+    }
+}
---
+++ js/Repositories/SubscriptionRepository.js
@@ -0,0 +1,25 @@
+class SubscriptionRepository extends BaseRepository {
+    constructor(storage) {
+        super(storage, 'subscriptions');
+    }
+
+    getUserSubcriptions(id) {
+        return this.getAllRaw().filter(c => c.id === id);
+    }
+
+    subscribeToUser(userId, targetUserId) {
+        const subs = this.getAllRaw();
+        if (!subs.some(s => s.userId === userId && s.targetUserId === targetUserId)) {
+            subs.push(new Subscription(this.generateId(subs), userId, targetUserId, null));
+            this.saveAll(subs);
+        }
+    }
+
+    subscribeToGroup(userId, targetGroupId) {
+        const subs = this.getAllRaw();
+        if (!subs.some(s => s.userId === userId && s.targetGroupId === targetGroupId)) {
+            subs.push(new Subscription(this.generateId(subs), userId, null, targetGroupId));
+            this.saveAll(subs);
+        }
+    }
+}
